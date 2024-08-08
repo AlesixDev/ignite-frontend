dee8ba6dfeb479f6fe2c71a1f3b51c2d56e5ba74
sellauth <sellauth@anzrac.com>
2024-08-08 19:18:09 +0000

    fix: use useGuildStore for guildchannel page

--- src/pages/GuildChannel/GuildChannel.jsx
+++ src/pages/GuildChannel/GuildChannel.jsx
@@ -1,18 +1,18 @@
 import { useEffect, useMemo } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import useStore from '../../hooks/useStore';
 import GuildLayout from '../../layouts/GuildLayout';
 import Channel from '../../components/Channel';
+import useGuildStore from '../../hooks/useGuildStore';
 
 const GuildChannelPage = () => {
-  const store = useStore();
+  const { guilds } = useGuildStore();
   const { navigate } = useNavigate();
 
   // get the guild id from the URL
   const { guildId } = useParams();
 
-  // find guild from store.guilds
-  const guild = useMemo(() => store.guilds.find((g) => g.id == guildId), [store.guilds, guildId]);
+  // find guild from guilds
+  const guild = useMemo(() => guilds.find((g) => g.id == guildId), [guilds, guildId]);
 
   // if no guild redirect away
   useEffect(() => {
