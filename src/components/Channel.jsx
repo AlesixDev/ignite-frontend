4bc0d20e1242348448ed7cbb87ea7a3b7f95e7c7
Patrik Kos <53878055+patriksh@users.noreply.github.com>
2024-08-12 10:06:13 +0200

    feat: message editing & deletion

--- src/components/Channel.jsx
+++ src/components/Channel.jsx
@@ -1,10 +1,13 @@
 import { useState, useCallback, useEffect, useRef, useMemo } from 'react';
 import { toast } from 'react-toastify';
-import { Gif, Gift, PlusCircle, Smiley, Sticker } from '@phosphor-icons/react';
+import { Gif, Gift, PlusCircle, Smiley, Sticker, NotePencil, Trash, ArrowBendUpLeft } from '@phosphor-icons/react';
 import api from '../api';
+import useStore from '../hooks/useStore';
 import ChannelBar from './ChannelBar.jsx';
 
-const ChannelMessage = ({ message, prevMessage }) => {
+const ChannelMessage = ({ message, prevMessage, setMessages, editingId, setEditingId }) => {
+  const store = useStore();
+
   const formattedDateTime = useMemo(() => {
     const date = new Date(message.created_at);
 
@@ -31,51 +34,146 @@ const ChannelMessage = ({ message, prevMessage }) => {
     }
   }, [prevMessage, message]);
 
-  return shouldStack ? (
-    <div className="flex">
-      <div className="w-14" />
-      <div className="flex flex-col items-start justify-start">
-        <div className="text-gray-400">{message.content}</div>
-      </div>
-    </div>
-  ) : (
-    <div className="mt-4 flex">
-      {message?.author.avatar ? (
-        <img
-          className="h-10 rounded-full bg-transparent"
-          src={message?.author.avatar}
-          alt="User avatar"
-        />
+  const isEditing = useMemo(() => editingId === message.id, [editingId, message.id]);
+  const canEdit = useMemo(() => message.author.id === store.user.id, [message.author.id, store.user.id]);
+  const canDelete = useMemo(() => message.author.id === store.user.id, [message.author.id, store.user.id]);
+
+  const [editedMessage, setEditedMessage] = useState(message.content);
+
+  const onEdit = useCallback(async (event) => {
+    event.preventDefault();
+
+    try {
+      await api.put(`/channels/${message.channel_id}/messages/${message.id}`, { content: editedMessage });
+      setMessages((messages) => messages.map((m) => m.id === message.id ? { ...m, content: editedMessage, updated_at: new Date().toISOString() } : m));
+      setEditingId(null);
+    } catch (error) {
+      console.error(error);
+      toast.error(error.response?.data?.message || 'Could not edit message.');
+    }
+  }, [message.channel_id, message.id, editedMessage, setMessages, setEditingId]);
+
+  const onDelete = useCallback(async () => {
+    try {
+      await api.delete(`/channels/${message.channel_id}/messages/${message.id}`);
+      setMessages((messages) => messages.filter((m) => m.id !== message.id));
+    } catch (error) {
+      console.error(error);
+      toast.error(error.response?.data?.message || 'Could not delete message.');
+    }
+  }, [message.channel_id, message.id, setMessages]);
+
+  const onReply = () => {
+
+  };
+
+  return (
+    <div className={`group relative py-0.5 ${isEditing ? 'bg-gray-800/60' : 'hover:bg-gray-800/60'} ${shouldStack ? '' : 'mt-3.5'}`}>
+      {shouldStack ? (
+        <div className="flex px-4">
+          <div className="w-14" />
+          <div className="flex flex-col items-start justify-start">
+            <div className="text-gray-400">{message.content}</div>
+          </div>
+        </div>
       ) : (
-        <div className="mr-4 flex size-10 items-center justify-center rounded-full bg-gray-800 text-gray-300">
-          {message?.author.username.slice(0, 1).toUpperCase()}
+        <div className="flex px-4">
+          {message?.author.avatar ? (
+            <img
+              className="h-10 rounded-full bg-transparent"
+              src={message?.author.avatar}
+              alt="User avatar"
+            />
+          ) : (
+            <div className="mr-4 flex size-10 items-center justify-center rounded-full bg-gray-800 text-gray-300">
+              {message?.author.username.slice(0, 1).toUpperCase()}
+            </div>
+          )}
+
+          <div className="flex flex-1 flex-col items-start justify-start">
+            <div className="mb-1 flex justify-start leading-none">
+              <h6 className="font-semibold leading-none">
+                {message?.author.username}
+              </h6>
+              <p className="ml-2 self-end text-xs font-medium leading-tight text-gray-600 dark:text-gray-500">
+                {formattedDateTime}
+              </p>
+            </div>
+
+            {isEditing ? (
+              <div className="my-2 w-full">
+                <div className="mb-1 flex items-center rounded-lg bg-gray-600 px-4 py-2">
+                  <form onSubmit={(e) => onEdit(e)} className="w-full">
+                    <input
+                      className="w-full border-0 bg-inherit p-0 text-white outline-none placeholder:text-gray-400 focus:ring-0"
+                      type="text"
+                      value={editedMessage}
+                      onChange={(e) => setEditedMessage(e.target.value)}
+                      autoFocus
+                    />
+                  </form>
+                </div>
+                <p className="text-xs text-gray-400">
+                  escape to <span className="text-primary">cancel</span>, enter to <span className="text-primary">save</span>
+                </p>
+              </div>
+            ) : (
+              <div className="text-gray-400">
+                {message.content}
+                {(message.updated_at && message.created_at !== message.updated_at) && (
+                  <span className="ml-1 text-[0.65rem] text-gray-500">(edited)</span>
+                )}
+              </div>
+            )}
+          </div>
         </div>
       )}
-
-      <div className="flex flex-1 flex-col items-start justify-start">
-        <div className="mb-1 flex justify-start leading-none">
-          <h6 className="font-semibold leading-none">
-            {message?.author.username}
-          </h6>
-          <p className="ml-2 self-end text-xs font-medium leading-tight text-gray-600 dark:text-gray-500">
-            {formattedDateTime}
-          </p>
+      {!isEditing && (
+        <div className="absolute -top-4 right-4 hidden rounded-md border border-gray-800 bg-gray-700 group-hover:flex">
+          {canEdit && (
+            <button type="button" onClick={() => setEditingId(message.id)} className="rounded-md p-2 text-sm text-white/90 hover:bg-primary/10 hover:text-primary">
+              <NotePencil className="size-5" />
+            </button>
+          )}
+          {canDelete && (
+            <button type="button" onClick={() => onDelete(message)} className="rounded-md p-2 text-sm text-white/90 hover:bg-primary/10 hover:text-primary">
+              <Trash className="size-5" />
+            </button>
+          )}
+          <button type="button" onClick={() => onReply(message)} className="rounded-md p-2 text-sm text-white/90 hover:bg-primary/10 hover:text-primary">
+            <ArrowBendUpLeft className="size-5" />
+          </button>
         </div>
-
-        <div className="text-gray-400">{message.content}</div>
-      </div>
+      )}
     </div>
   );
 };
 
-const ChannelMessages = ({ messages, messagesRef }) => {
+const ChannelMessages = ({ messages, setMessages, messagesRef }) => {
+  const [editingId, setEditingId] = useState(null);
+
+  // attach escape key listener to cancel editing
+  useEffect(() => {
+    const handleEscape = (event) => {
+      if (event.key === 'Escape') {
+        setEditingId(null);
+      }
+    };
+
+    window.addEventListener('keydown', handleEscape);
+
+    return () => {
+      window.removeEventListener('keydown', handleEscape);
+    };
+  }, []);
+
   return (
-    <div className="h-full max-h-[calc(100vh-9rem)] overflow-y-auto px-4" ref={messagesRef}>
+    <div className="h-full max-h-[calc(100vh-9rem)] overflow-y-auto" ref={messagesRef}>
       {messages.map((message, index) => {
         const prevMessage = messages[index - 1] || null;
 
         return (
-          <ChannelMessage key={message.id} message={message} prevMessage={prevMessage} />
+          <ChannelMessage key={message.id} message={message} prevMessage={prevMessage} setMessages={setMessages} editingId={editingId} setEditingId={setEditingId} />
         );
       })}
     </div>
@@ -103,7 +201,7 @@ const ChannelInput = ({ channel, scrollToBottom }) => {
   }, [message, channel?.channel_id, scrollToBottom]);
 
   return (
-    <div className="mx-4 my-6 flex items-center rounded-lg bg-gray-300 py-2 dark:bg-gray-600">
+    <div className="mx-4 my-6 flex items-center rounded-lg bg-gray-600 py-2">
       <div>
         <PlusCircle
           className="mx-4 cursor-pointer text-gray-400 hover:text-gray-200"
@@ -196,13 +294,9 @@ const Channel = ({ channel }) => {
     window.Echo.private(`channel.${channel.channel_id}`)
       .listen('.message.created', (event) => {
         if (event.channel.id == channel.channel_id) {
-          console.log('Received message:', event);
           setMessages((messages) => [...messages, event.message]);
         }
       })
-
-    // Assuming successful subscription if no error is caught
-    console.log(`Successfully subscribed to channel.${channel.channel_id}`);
 
     return () => {
       window.Echo.leave(`channel.${channel.channel_id}`);
@@ -213,7 +307,7 @@ const Channel = ({ channel }) => {
     <div className="relative flex w-full flex-col dark:bg-gray-700">
       <ChannelBar channel={channel} />
       <hr className="m-0 w-full border border-gray-800 bg-gray-800 p-0" />
-      <ChannelMessages messages={messages} messagesRef={messagesRef} />
+      <ChannelMessages messages={messages} setMessages={setMessages} messagesRef={messagesRef} />
       <ChannelInput channel={channel} fetchMessages={fetchMessages} scrollToBottom={scrollToBottom} />
     </div>
   );
